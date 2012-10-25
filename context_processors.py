25be0bc0554c3f53d8526f0006d6184b9780df8c
Alfred Chiesa <alfred@villaroad.com>
2012-10-25 18:38:08 -0400

    build for the brydges

---
+++ context_processors.py
@@ -0,0 +1,31 @@
+from timepiece import models as timepiece
+from timepiece import utils
+from timepiece.forms import QuickSearchForm
+
+
+def quick_search(request):
+    return {
+        'quick_search_form': QuickSearchForm(),
+    }
+
+
+def active_entries(request):
+    active_entries = None
+
+    if request.user.is_authenticated():
+        active_entries = timepiece.Entry.objects.filter(
+            end_time__isnull=True,
+        ).exclude(
+            user=request.user,
+        ).select_related('user', 'project', 'activity')
+
+    return {
+        'active_entries': active_entries,
+    }
+
+
+def extra_nav(request):
+    context = {
+        'timepiece_extra_nav': utils.get_setting('TIMEPIECE_EXTRA_NAV'),
+    }
+    return context
